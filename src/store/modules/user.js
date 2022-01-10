ff441e9f2ae69e93afd6ce482fd11aea0f5b6c8d
Username <Username@example.com>
2022-01-10 13:10:18 +0800

    47.element-ui框架开发流程介绍

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -30,61 +30,85 @@ const mutations = {
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+    const data = { 'token': 'admin' }
+
+    setToken(data.token)// 将token存储在cookie中
+    commit('SET_TOKEN', data.token)
+
+    // const { username, password } = userInfo
+    // return new Promise((resolve, reject) => {
+    //   login({ username: username.trim(), password: password }).then(response => {
+    //     const { data } = response
+    //     commit('SET_TOKEN', data.token)
+    //     setToken(data.token)
+    //     resolve()
+    //   }).catch(error => {
+    //     reject(error)
+    //   })
+    // })
   },
 
   // get user info
   getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
+    // const data  = {'roles':'admin','name':'admin','avatar':'http://localhost:9528/?imageView2/1/w/80/h/80'}
+    const data = {
+      'roles': ['admin'],
+      'name': 'admin',
+      'avatar': 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
+    }
+    if (data.roles && data.roles.length > 0) { // 验证返回的是roles是否是一个非空数组
+    //  非空数组
+      commit('SET_ROLES', data.roles)
+    } else {
+      reject('getInfo:roles must be a non-null array !')
+    }
+    commit('SET_NAME', data.name)
+    commit('SET_AVATAR', data.avatar)
 
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
+    // return new Promise((resolve, reject) => {
+    //   getInfo(state.token).then(response => {
+    //     const { data } = response
 
-        const { name, avatar } = data
+    //     if (!data) {
+    //       return reject('Verification failed, please Login again.')
+    //     }
 
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
+    //     const { name, avatar } = data
+
+    //     commit('SET_NAME', name)
+    //     commit('SET_AVATAR', avatar)
+    //     resolve(data)
+    //   }).catch(error => {
+    //     reject(error)
+    //   })
+    // })
   },
 
   // user logout
   logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
+    commit('SET_TOKEN', '')
+    commit('SET_ROLES', [])
+    removeToken
+
+    // return new Promise((resolve, reject) => {
+    //   logout(state.token).then(() => {
+    //     removeToken() // must remove  token  first
+    //     resetRouter()
+    //     commit('RESET_STATE')
+    //     resolve()
+    //   }).catch(error => {
+    //     reject(error)
+    //   })
+    // })
   },
 
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
+  // remove token 前端 登出
+  resetToken({ commit }) { // FedLogOut
+    // return new Promise(resolve => {
+    removeToken() // must remove  token  first
+    commit('RESET_STATE')
+    resolve()
+    // })
   }
 }
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -19,7 +19,8 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      // config.headers['X-Token'] = getToken()
+      config.headers['token'] = getToken()// 让每个请求携带自定义token,请求modules/user.js的tolen
     }
     return config
   },
